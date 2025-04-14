90cd3c3c952a2bbc7e3ac226e40903e0ccee32a9
tomoki-ucd <tomoki.ucd@gmail.com>
2025-04-15 01:32:59 +0900

    Implement ObjectPlacementManager class.

--- Assets/Scripts/ObjectPlacementManager.cs
+++ Assets/Scripts/ObjectPlacementManager.cs
@@ -1,16 +1,61 @@
 using UnityEngine;
+using Meta.XR;
+using Meta.XR.MRUtilityKit;
+using Meta.XR.EnvironmentDepth;
 
+/// <summary>
+/// Provides the functionality to place objects using ray
+/// </summary>
 public class ObjectPlacementManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private EnvironmentRaycastManager _raycastManager;  // This class uses Depth API to provide raycasting against the physical environment.
+    [SerializeField] private Transform _rightControllerAnchor;
+    [SerializeField] private GameObject _objectToSpawn;
+
+    void Awake()
     {
-        
+        if(EnvironmentRaycastManager.IsSupported != true)
+        {
+            Debug.Log($"[{this.name}] EnvironmentRaycastManager is not supported.");
+        }
+        else
+        {
+            Debug.Log($"[{this.name}] EnvironmentRaycastManager is supported.");
+            // This will also add the EnvironmentDepthManager instance if not present in the scene.
+            // Disable EnvironemtDepthManager manually when disable EnvironemtRaycastManager.
+            _raycastManager = gameObject.AddComponent<EnvironmentRaycastManager>();
+        }
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if(OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
+        {
+            // Create a standard Unity ray
+            var ray = new Ray(_rightControllerAnchor.position, _rightControllerAnchor.forward); // Ray(from, to)
+            if(_raycastManager.Raycast(ray, out var hit))
+            {
+                // Spawn an object
+                var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+//                var obj = _objectToSpawn;
+                obj.transform.SetPositionAndRotation(hit.point, Quaternion.LookRotation(hit.normal, Vector3.up));  // SetPositionAndRotation(position, rotation)
+                                                                                                                    // LookRotation(forward, upward). forward is prioritized.
+                obj.transform.localScale *= 0.1f;                                                                                                                    
+                //If MRUK's world locking is not active, Add spatial anchor to the objs's position. 
+                if(MRUK.Instance != null)
+                {
+                    if(MRUK.Instance.IsWorldLockActive != true)
+                    {
+                        Debug.Log($"[{this.name}] WorldLock is not set active.");
+                        obj.AddComponent<OVRSpatialAnchor>();
+                    }
+                }
+                else
+                {
+                    Debug.Log($"[{this.name}] No MRUK Instance.");
+                }
+            }
+        }
     }
 }
