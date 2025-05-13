e8fb65e3cb0695d1ace71392a0363b1bd2ddf7eb
tomoki-ucd <tomoki.ucd@gmail.com>
2025-05-14 00:35:35 +0900

    Modify the spawned position of furniture prefabs.

--- Assets/Scripts/PrefabInstantiator.cs
+++ Assets/Scripts/PrefabInstantiator.cs
@@ -1,4 +1,3 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PrefabInstantiator: MonoBehaviour
@@ -20,14 +19,28 @@ public class PrefabInstantiator: MonoBehaviour
         }
 
         Transform spawnPoint= canvas.transform;
-//        spawnPoint.position.x += 2.0f;
         Vector3 spawnPosition = spawnPoint.position;
         spawnPosition.x += 2.0f;
 
         Vector3 position = spawnPosition != null ? spawnPosition: Vector3.zero;
 //        Quaternion quaternion = spawnPoint != null ? spawnPoint.rotation : Quaternion.identity;
-        Quaternion quaternion = Quaternion.identity;
+//        Quaternion quaternion = Quaternion.identity;  // This sets the rotation 0,0,0 against its parent object.
 
-        Instantiate(prefabToInstantiate, position, quaternion);
+//        Instantiate(prefabToInstantiate, position, quaternion);
+        GameObject spawnedInstance = Instantiate(prefabToInstantiate, position, prefabToInstantiate.transform.rotation);
+
+
+        // Adjust the height of the spawned object by moving it downward by half of its own height.
+        Collider objectCollider = spawnedInstance.GetComponent<Collider>(); // This will get ANY type of Collider including MeshCollider
+        if(objectCollider != null)
+        {
+            float halfHeight = objectCollider.bounds.size.y / 2f;
+            spawnedInstance.transform.position -= new Vector3(0, halfHeight, 0);
+            Debug.Log($"[{this.name}] Spawned object {spawnedInstance} is lowered by {halfHeight}.");
+        }
+        else
+        {
+            Debug.LogError($"[{this.name}] Spawned object {spawnedInstance} has no Collider. Cannot determine the height to lower it.");
+        }
     }
 }
