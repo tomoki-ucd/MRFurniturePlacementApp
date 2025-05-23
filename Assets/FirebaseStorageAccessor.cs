b5af400bbe0bc9a982662a7bd19682528f99625f
tomoki-ucd <tomoki.ucd@gmail.com>
2025-05-23 14:23:33 +0900

    [WIP] Implement FirebaseStorageAccessor.

--- Assets/FirebaseStorageAccessor.cs
+++ Assets/FirebaseStorageAccessor.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
 using Firebase.Storage;
+using Firebase.Extensions;
 
 public class FirebaseStorageAccessor : MonoBehaviour
 {
+    private void Start()
+    {
+        RetrieveFurnitureData();
+    }
+
     public void RetrieveFurnitureData()
     {
         // Get a reference to the storage service, using the default Firebase App
@@ -24,5 +30,21 @@ public class FirebaseStorageAccessor : MonoBehaviour
         // Create a fullpath instead of using child reference
         StorageReference bookshelfFullPathRef = storageRef.Child("prefabs/bookshelf.prefab");
 
+        // Navigation with Parent, Child and Root is supported. I don't explain it here.
+
+        // You can get Reference path and name in string.
+        // string path = bookshelfRef.Path; // "prefabs/bookshelf.prefab"
+        // string name = bookshelfRef.Name; // "bookshelf.prefab"
+        // string bucketPath = bookshelfRef.Bucket;
+
+        // Download the file
+        // GetDownloadUrlAsyncはURLをダウンロードしているだけで、ファイルをダウンロードしているわけではない
+        // storageRefをURLに変換するにはGetDownloadUrlAsyncを使う
+        storageRef.GetDownloadUrlAsync().ContinueWithOnMainThread(task => {
+            if (!task.IsFaulted && !task.IsCanceled)
+            {
+                Debug.Log("Download URL:" + task.Result);
+            }
+        });
     }
 }
